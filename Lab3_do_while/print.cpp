3ea04257ba6303b348437d33101ff7fdeabbb468
alki <zif.off@gmail.com>
2021-03-10 21:45:30 +0300

    fix do...while

--- Lab3_do_while/print.cpp
+++ Lab3_do_while/print.cpp
@@ -4,17 +4,20 @@ void print(int n, int k) // Задание 3
 {
 	int sch1 = 0, sch2 = 0;
 
-	do
-	{
-		++sch2;
-		if (sch2 % k == 0) {
-			++sch1;
-			continue;
-		}
-		else {
-			std::cout << a(sch1) << " ";
-			++sch1;
-		}
+	if (n != 0) {
+		do
+		{
+			++sch2;
+			if (sch2 % k == 0) {
+				++sch1;
+				continue;
+			}
+			else {
+				std::cout << a(sch1) << " ";
+				++sch1;
+			}
 
-	} while (sch1 < n);
+		} while (sch1 < n);
+	}
+	
 }
--- Lab3_while/print.cpp
+++ Lab3_while/print.cpp
@@ -4,7 +4,7 @@ void print(int n, int k) // Задание 3
 {
 	int sch1 = 0, sch2 = 0;
 
-	while (sch1 <= n)
+	while (sch1 < n)
 	{
 		++sch2;
 		if (sch2 % k == 0) {
